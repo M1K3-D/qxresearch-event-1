d6e9ead743f8a34e13523ff11709d2e66397ea96
M1K3-D <dufosse.com@gmail.com>
2022-11-02 18:10:44 +0100

    Commit 1

---
+++ Listbox.py
@@ -0,0 +1,33 @@
+import os
+import tkinter as tk
+from tkinter.filedialog import askdirectory
+#from tkinter.messagebox import showinfo
+win = tk.Tk()
+win.title("Browser")
+win.geometry("800x800")
+#def openfile(evt): pass
+
+homedrive = os.environ['PWD']
+#rep_source = tk.filedialog.askdirectory()
+
+def ask_question():
+    listbox.delete(0,tk.END)
+    rep_source = tk.filedialog.askdirectory(parent=win, initialdir=homedrive, title="Selectionnez le dossier SOURCE")
+    for file in os.listdir(rep_source):
+        listbox.insert(0, file)
+#        print (file)
+
+button = tk.Button(win, text="Open a File", command=ask_question)
+button.pack()
+listbox = tk.Listbox(win)
+listbox.pack(expand=tk.YES, fill=tk.BOTH)
+# listbox.bind("<<ListBoxSelect>>",openfile)
+#rep_source = tk.filedialog.askdirectory( initialdir = homedrive, title = "Selectionnez le dossier SOURCE")
+#rep_source = homedrive
+
+# for file in os.listdir("/Users/mdufosse/Documents/AData/Dev/Python/Prog"):
+
+#rep_source = tk.filedialog.askdirectory(parent=win, initialdir=homedrive, title="Selectionnez le dossier SOURCE")
+
+
+win.mainloop()
